6c5f195a113e332610b0504c80fe469d2c8ad87e
sbaamoha <sbaamohe@gmail.com>
2023-09-07 13:38:51 +0100

    merchants done

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,17 +1,9 @@
 import express, { Router } from "express";
-import GoogleMerchantService from "../services/googleMerchant";
-
+// import GoogleMerchantService from "../services/googleMerchant";
 import GetProductsService from "../services/getProducts";
 import fs from "fs";
 import { parse } from "json2csv";
-<<<<<<< Updated upstream
-import { json2xml, js2xml } from "xml-js";
-
-// import { xml2js } from "xml2js";
-// const jsonfeedToRSS = require("jsonfeed-to-rss");
-=======
-import { js2xml } from "xml-js";
->>>>>>> Stashed changes
+import xml2js from "xml2js";
 export default () => {
   const router = Router();
   router.use(express.json());
@@ -28,16 +20,43 @@ export default () => {
       const jsonProducts = await getProductsService.getProducts(
         user.username || "google",
       );
-      // const products =
-      console.log(jsonProducts);
-      const xml = js2xml(jsonProducts, { compact: true, spaces: 4 });
+      const builder = new xml2js.Builder({
+        headless: false,
+        cdata: true,
+        renderOpts: {
+          pretty: true,
+          indent: " ",
+          newline: "\n",
+        },
+      });
+      const xmlData = {
+        rss: {
+          $: {
+            "xmlns:g": "http://base.google.com/ns/1.0",
+            version: "2.0",
+          },
+          channel: {
+            title: "متجر المنتجات المغربية",
+            link: "https://maroc4products.com/ar",
+            description:
+              "اكتشف المنتجات المغربية الأصيلة في متجرنا! نحن نقدم مجموعة متنوعة من العناصر بما في ذلك منتجات العناية الشخصية والمواد الغذائية والعسل المغربي والأملو والأعشاب. ستساعدك مجموعة منتجاتنا الفريدة",
+            item: jsonProducts.map((product) => ({
+              "g:id": product.id,
+              "g:title": product.title,
+              "g:description": product.description,
+              "g:link": product.link,
+              "g:image_link": product.image_link,
+              "g:condition": product.condition,
+              "g:price": product.price,
+              "g:availability": product.availability,
+              "g:category": product.category,
+            })),
+          },
+        },
+      };
 
-      let edits = `<?xml version="1.0"?> \n <rss xmlns:g="http://base.google.com/ns/1.0"> \n <channel> \n <title>متجر المنتجات المغربية</title>
-      <link>https://maroc4products.com/ar</link>
-      <description>اكتشف المنتجات المغربية الأصيلة في متجرنا! نحن نقدم مجموعة متنوعة من العناصر بما في ذلك منتجات العناية الشخصية والمواد الغذائية والعسل المغربي والأملو والأعشاب. ستساعدك مجموعة منتجاتنا الفريدة</description>
-      ${xml} \n </channel> \n </rss>`;
-
-      fs.writeFileSync("productsXml.xml", edits);
+      const xml = builder.buildObject(xmlData);
+      fs.writeFileSync("productsXml.xml", xml);
       res.download("productsXml.xml", "productsXml.xml", (err) => {
         if (err) {
           console.error("Error sending file:", err);
@@ -72,90 +91,28 @@ export default () => {
       res.status(500).json(error.message);
     }
   });
-  router.get("/grab-products-google", async (req, res) => {
-    try {
-      const getProductsService: GetProductsService =
-        req.scope.resolve("getProductsService");
-      // grab all products from database
-      const jsonProducts = await getProductsService.getProducts("google");
-      const csv = parse(jsonProducts as any);
-      console.log(csv);
-      // const json = JSON.stringify(jsonProducts);
+  // router.post("/create-multi-products-for-google", async (req, res) => {
+  //   try {
+  //     const GoogleMerchant: GoogleMerchantService = req.scope.resolve(
+  //       "googleMerchantService",
+  //     );
 
-      // const xml = js2xml(jsonProducts, {
-      //   compact: true,
-      //   spaces: 4,
-      //   ignoreComment: true,
-      // });
-      // const xml = xml2js.parse(jsonProducts);
-      function csvToXml(csvString, csvHeaders) {
-        const csv = csvString.split("\n")[0];
-        const csvRows = csv.split(",");
-        console.log(csv);
-        let xml = `<products>`;
+  //     const { products } = req.body; // Assuming the request body contains user data
+  //     const googleProduct = await GoogleMerchant.insertMultiProducts(products);
+  //     // const googleProduct = await GoogleMerchant.syncProductToMerchantCenter(
+  //     //   products[0],
+  //     // );
 
-        for (const row of csvRows) {
-          const product = {};
-
-          for (let i = 0; i < csvHeaders.length; i++) {
-            product[csvHeaders[i]] = row[i];
-          }
-
-          xml += `<product>`;
-          for (const key in product) {
-            xml += `<${key}>${product[key]}</${key}>`;
-          }
-          xml += `</product>`;
-        }
-
-        xml += `</products>`;
-
-        return xml;
-      }
-
-      fs.writeFileSync(
-        "products.xml",
-        csvToXml(csv, Object.keys(jsonProducts[0])),
-      );
-      res.download("products.xml", "products.xml", (err) => {
-        if (err) {
-          console.error("Error sending file:", err);
-          res.status(500).send("Internal Server Error");
-        }
-      });
-    } catch (error) {
-      res.status(500).json(error.message);
-    }
-  });
-  router.post("/create-multi-products-for-google", async (req, res) => {
-    try {
-      const GoogleMerchant: GoogleMerchantService = req.scope.resolve(
-        "googleMerchantService",
-      );
-
-<<<<<<< Updated upstream
-      const newProducts = req.body; // Assuming the request body contains user data
-      const googleProduct = await GoogleMerchant.insertMultiProducts(
-        newProducts,
-      );
-=======
-      const { products } = req.body; // Assuming the request body contains user data
-      const googleProduct = await GoogleMerchant.insertMultiProducts(products);
-      // const googleProduct = await GoogleMerchant.syncProductToMerchantCenter(
-      //   products[0],
-      // );
->>>>>>> Stashed changes
-
-      if (!products) {
-        throw new Error("no product received");
-      }
-      if (!googleProduct) {
-        throw new Error("product not published in google merchant center");
-      }
-      res.status(201).json(googleProduct);
-    } catch (error) {
-      res.status(500).json(error.message);
-    }
-  });
+  //     if (!products) {
+  //       throw new Error("no product received");
+  //     }
+  //     if (!googleProduct) {
+  //       throw new Error("product not published in google merchant center");
+  //     }
+  //     res.status(201).json(googleProduct);
+  //   } catch (error) {
+  //     res.status(500).json(error.message);
+  //   }
+  // });
   return router;
 };
--- src/services/getProducts.ts
+++ src/services/getProducts.ts
@@ -8,30 +8,17 @@ class GetProductsService extends TransactionBaseService {
   }
   makeTiktokProduct(product) {
     return {
-      sku_id: 122,
-      title: "product.title",
-      description: "product.description",
-      price: "22 SAR",
-      link: "https://maroc4products.com",
-      availability: "in stock",
-      image_link: "https://picsum.photos/200/300",
-      condition: "new",
-      // currency: product.currency,
-      category: "skin care",
-      mpn: "G34423E",
-      brand: "nike",
-      // sku_id: product.sku || product.id,
-      // title: product.title,
-      // description: product.description,
-      // price: `${product.price} ${product.currency}`,
-      // link: product.link,
-      // availability: product.status,
-      // image_link: product.imageLink,
-      // condition: product.condition,
-      // // currency: product.currency,
-      // category: product.category,
-      // mpn: product.handle,
-      // brand: product.brand,
+      sku_id: product.sku || product.id,
+      title: product.title,
+      description: product.description,
+      price: `${product.price} ${product.currency}`,
+      link: product.link,
+      availability: product.status,
+      image_link: product.imageLink,
+      condition: product.condition,
+      category: product.category,
+      mpn: product.handle,
+      brand: product.brand,
     };
   }
   makeYandexProduct(product) {
@@ -42,58 +29,34 @@ class GetProductsService extends TransactionBaseService {
       Price: product.price,
       URL: product.link,
       Image: product.image_url,
-      Currency: "USD",
+      Currency: "USD", // default
     };
   }
   makeGoogleProduct(product) {
     return {
-      id: 122,
-      title: "product.title",
-      description: "product.description",
-      price: "22 SAR",
-      link: "https://maroc4products.com",
-      availability: "in_stock",
-      image_link: "https://picsum.photos/200/300",
-      condition: "new",
-      brand: "nike",
-      // currency: product.currency,
-      // category: "skin care",
-      // id: product.id,
-      // title: product.title,
-      // description: product.description,
-      // price: `${product.price} ${product.currency}`,
-      // link: product.link,
-      // availability: product.availability,
-      // image_link: product.image_url,
-      // condition: product.condition || "new",
-      // // currency: product.currency,
-      // category: product.category,
-      mpn: "GG4343DLS2",
+      id: product.id,
+      title: product.title,
+      description: product.description,
+      price: `${product.price} ${product.currency}`,
+      link: product.url,
+      availability: product.availability,
+      image_link: product.image_url,
+      condition: product.condition || "new",
+      category: product.category,
+      mpn: product.mpn,
     };
   }
   makeFacebookProduct(product) {
     return {
-      id: 122,
-      title: "product.title",
-      description: "product.description",
-      price: "22 SAR",
-      link: "https://maroc4products.com",
-      availability: "in stock",
-      image_link: "https://picsum.photos/200/300",
-      condition: "new",
-      // currency: product.currency,
-      // category: "skin care",
-      brand: "nike",
-      // id: product.id,
-      // title: product.title,
-      // description: product.description,
-      // price: `${product.price} ${product.currency}`,
-      // link: product.link,
-      // availability: product.availability,
-      // image_link: product.image_url,
-      // condition: product.condition || "new",
-      // // currency: product.currency,
-      // brand: product.brand,
+      id: product.id,
+      title: product.title,
+      description: product.description,
+      price: `${product.price} ${product.currency}`,
+      link: product.url,
+      availability: product.availability,
+      image_link: product.image_url,
+      condition: product.condition || "new",
+      brand: product.brand,
     };
   }
 
@@ -101,36 +64,6 @@ class GetProductsService extends TransactionBaseService {
     try {
       // fetch products here and name them items
       const items = await this.productService.list({});
-      // const items = [
-      //   {
-      //     id: 1,
-      //     title: "product 2",
-      //     description: " a good desc",
-      //     brand: "nike",
-      //     price: 15,
-      //     currency: "SAR",
-      //     link: "https://google.com",
-      //     image_url: "http://www.example.com/image1.jpg",
-      //     condition: "new",
-      //     availability: "in_stock",
-      //     category: "skin care",
-      //     mpn: "GO12345OOGLE",
-      //   },
-      //   {
-      //     title: "testing product",
-      //     id: 2,
-      //     brand: "nike",
-      //     description: " a good desc",
-      //     price: 13,
-      //     mpn: "GO12345OOGLE",
-      //     currency: "SAR",
-      //     link: "https://google.com",
-      //     image_url: "http://www.example.com/image1.jpg",
-      //     condition: "new",
-      //     availability: "in_stock",
-      //     category: "skin care",
-      //   },
-      // ];
 
       if (platform === "meta") {
         const products = items.map((product) => {
@@ -139,67 +72,10 @@ class GetProductsService extends TransactionBaseService {
         return products;
       }
       if (platform === "google") {
-<<<<<<< Updated upstream
-        const products = [
-          this.makeGoogleProduct(items[0]),
-          {
-            id: 23432,
-            title: "product.name",
-            description: "product.description",
-            price: "92 SAR",
-            link: "https://maroc4products.com",
-            availability: "in_stock",
-            image_link: "https://picsum.photos/200/300",
-            condition: "new",
-            brand: "nike",
-            mpn: "HHGD82839S",
-          },
-        ];
-        // const products = items.map((product) => {
-        // return this.makeGoogleProduct(items[0]);
-        // });
+        const products = items.map((product) => {
+          return this.makeGoogleProduct(product);
+        });
         return products;
-=======
-        // const products = items.map((product) => {
-        //   return this.makeGoogleProduct(product);
-        // });
-        // return products;
-        const products = [
-          {
-            title: "tshirt one",
-            id: 1,
-            description: "product description",
-            brand: "nike",
-            availability: "in_stock",
-            condition: "new",
-            targetCountry: "US",
-            image_url: "https://picsum.photos/200/300",
-            url: "https://maroc4products.com",
-            price: 99,
-            currency: "SAR",
-            category: "skin",
-            contentLanguage: "ar",
-            channel: "online",
-          },
-          {
-            title: "Tshirt 3",
-            id: 2,
-            description: "product. description",
-            brand: "nike",
-            availability: "in_stock",
-            condition: "new",
-            targetCountry: "US",
-            image_url: "https://picsum.photos/200/300",
-            url: "https://maroc4products.com",
-            price: 209,
-            currency: "SAR",
-            googleProductCategory: "skin",
-            contentLanguage: "ar",
-            channel: "online",
-          },
-        ];
-        return products.map((product) => this.makeGoogleProduct(product));
->>>>>>> Stashed changes
       }
       if (platform === "bing") {
         const products = items.map((product) => {
